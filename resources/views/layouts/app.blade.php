e01d46350bd3a069d89e4b8fd61b71f0813e3114
Ivontsoa Gracia <graciaandriamihamina@gmail.com>
2025-03-19 12:00:09 +0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>@yield('title')</title>
 
-        <!-- <title>{{ config('app.name', 'Laravel') }}</title> -->
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
